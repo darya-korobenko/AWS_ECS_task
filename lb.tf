ca52cf14089b3e1860d76184a3392db0f416dd09
Daria <kdo1404p@ukr.net>
2023-09-11 03:11:13 +0300

    minor fix

--- lb.tf
+++ lb.tf
@@ -10,8 +10,8 @@ resource "aws_alb" "application_load_balancer" {
 
 resource "aws_security_group" "load_balancer_security_group" {
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # allow traffic in from all sources
   }
